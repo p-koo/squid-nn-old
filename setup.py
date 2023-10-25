b23433947d0e0aa6d0ffd2781b2e7d19c8744856
Evan Seitz <evan.e.seitz@gmail.com>
2023-10-25 16:18:04 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="squid-nn",
-    version="0.1.0",
+    version="0.1.1",
     author="Evan Seitz",
     author_email="evan.e.seitz@gmail.com",
     packages=find_packages(),
